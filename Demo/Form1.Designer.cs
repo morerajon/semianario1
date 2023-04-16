8003c3217c82c61bcd7a651ef5f46fff7298f3c5
jmorera <morerajonathanisfd41@gmail.com>
2023-04-15 23:37:44 -0300

    practica de seminario 1

---
+++ Demo/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSi = new System.Windows.Forms.Button();
+            this.btnNo = new System.Windows.Forms.Button();
+            this.btnABC = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnEjercicio1_3 = new System.Windows.Forms.Button();
+            this.btnEjer1_2 = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnvisible = new System.Windows.Forms.Button();
+            this.btninvisible = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnDespedida = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSi
+            // 
+            this.btnSi.BackColor = System.Drawing.Color.YellowGreen;
+            this.btnSi.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSi.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSi.Location = new System.Drawing.Point(135, 145);
+            this.btnSi.Name = "btnSi";
+            this.btnSi.Size = new System.Drawing.Size(80, 50);
+            this.btnSi.TabIndex = 0;
+            this.btnSi.Text = "SI";
+            this.btnSi.UseVisualStyleBackColor = false;
+            this.btnSi.Click += new System.EventHandler(this.btnSi_Click);
+            // 
+            // btnNo
+            // 
+            this.btnNo.BackColor = System.Drawing.Color.Crimson;
+            this.btnNo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNo.Location = new System.Drawing.Point(268, 145);
+            this.btnNo.Name = "btnNo";
+            this.btnNo.Size = new System.Drawing.Size(80, 50);
+            this.btnNo.TabIndex = 1;
+            this.btnNo.Text = "No";
+            this.btnNo.UseVisualStyleBackColor = false;
+            this.btnNo.Click += new System.EventHandler(this.btnNo_Click);
+            // 
+            // btnABC
+            // 
+            this.btnABC.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnABC.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnABC.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnABC.Location = new System.Drawing.Point(393, 145);
+            this.btnABC.Name = "btnABC";
+            this.btnABC.Size = new System.Drawing.Size(80, 50);
+            this.btnABC.TabIndex = 2;
+            this.btnABC.Text = "A,B,C";
+            this.btnABC.UseVisualStyleBackColor = false;
+            this.btnABC.Click += new System.EventHandler(this.btnABC_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.btnEjercicio1_3);
+            this.panel1.Controls.Add(this.btnEjer1_2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(113, 261);
+            this.panel1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Gold;
+            this.button1.FlatAppearance.BorderSize = 2;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(0, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 29);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Ejercicio 1.4";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnEjercicio1_3
+            // 
+            this.btnEjercicio1_3.BackColor = System.Drawing.SystemColors.ControlText;
+            this.btnEjercicio1_3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnEjercicio1_3.FlatAppearance.BorderColor = System.Drawing.Color.Gold;
+            this.btnEjercicio1_3.FlatAppearance.BorderSize = 2;
+            this.btnEjercicio1_3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEjercicio1_3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEjercicio1_3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnEjercicio1_3.Location = new System.Drawing.Point(0, 30);
+            this.btnEjercicio1_3.Name = "btnEjercicio1_3";
+            this.btnEjercicio1_3.Size = new System.Drawing.Size(113, 29);
+            this.btnEjercicio1_3.TabIndex = 1;
+            this.btnEjercicio1_3.Text = "Ejercicio 1.3";
+            this.btnEjercicio1_3.UseVisualStyleBackColor = false;
+            this.btnEjercicio1_3.Click += new System.EventHandler(this.btnEjercicio1_3_Click);
+            // 
+            // btnEjer1_2
+            // 
+            this.btnEjer1_2.BackColor = System.Drawing.Color.Black;
+            this.btnEjer1_2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnEjer1_2.FlatAppearance.BorderColor = System.Drawing.Color.Gold;
+            this.btnEjer1_2.FlatAppearance.BorderSize = 2;
+            this.btnEjer1_2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEjer1_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEjer1_2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnEjer1_2.Location = new System.Drawing.Point(0, 0);
+            this.btnEjer1_2.Name = "btnEjer1_2";
+            this.btnEjer1_2.Size = new System.Drawing.Size(113, 30);
+            this.btnEjer1_2.TabIndex = 0;
+            this.btnEjer1_2.Text = "Ejercicio 1.2";
+            this.btnEjer1_2.UseVisualStyleBackColor = false;
+            this.btnEjer1_2.Click += new System.EventHandler(this.btnEjer1_2_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.Red;
+            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.ForeColor = System.Drawing.Color.Snow;
+            this.btnSalir.Location = new System.Drawing.Point(463, 0);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(40, 30);
+            this.btnSalir.TabIndex = 4;
+            this.btnSalir.Text = "X";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnvisible
+            // 
+            this.btnvisible.BackColor = System.Drawing.SystemColors.ControlText;
+            this.btnvisible.FlatAppearance.BorderColor = System.Drawing.Color.Chartreuse;
+            this.btnvisible.FlatAppearance.BorderSize = 2;
+            this.btnvisible.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnvisible.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnvisible.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnvisible.Location = new System.Drawing.Point(174, 132);
+            this.btnvisible.Name = "btnvisible";
+            this.btnvisible.Size = new System.Drawing.Size(112, 26);
+            this.btnvisible.TabIndex = 2;
+            this.btnvisible.Text = "Labels visibles";
+            this.btnvisible.UseVisualStyleBackColor = false;
+            this.btnvisible.Visible = false;
+            this.btnvisible.Click += new System.EventHandler(this.btnvisible_Click);
+            // 
+            // btninvisible
+            // 
+            this.btninvisible.BackColor = System.Drawing.SystemColors.ControlText;
+            this.btninvisible.FlatAppearance.BorderColor = System.Drawing.Color.Chartreuse;
+            this.btninvisible.FlatAppearance.BorderSize = 2;
+            this.btninvisible.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btninvisible.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btninvisible.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btninvisible.Location = new System.Drawing.Point(326, 132);
+            this.btninvisible.Name = "btninvisible";
+            this.btninvisible.Size = new System.Drawing.Size(113, 27);
+            this.btninvisible.TabIndex = 3;
+            this.btninvisible.Text = "Labels invisibles";
+            this.btninvisible.UseVisualStyleBackColor = false;
+            this.btninvisible.Visible = false;
+            this.btninvisible.Click += new System.EventHandler(this.btninvisible_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(159, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 36);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MV Boli", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Navy;
+            this.label2.Location = new System.Drawing.Point(260, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 46);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Monotype Corsiva", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label3.Location = new System.Drawing.Point(385, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 43);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "C";
+            // 
+            // btnDespedida
+            // 
+            this.btnDespedida.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.btnDespedida.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDespedida.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDespedida.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnDespedida.Location = new System.Drawing.Point(243, 205);
+            this.btnDespedida.Name = "btnDespedida";
+            this.btnDespedida.Size = new System.Drawing.Size(116, 44);
+            this.btnDespedida.TabIndex = 8;
+            this.btnDespedida.Text = "Despedida";
+            this.btnDespedida.UseVisualStyleBackColor = false;
+            this.btnDespedida.Visible = false;
+            this.btnDespedida.Click += new System.EventHandler(this.btnDespedida_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(503, 261);
+            this.Controls.Add(this.btnDespedida);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btninvisible);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnvisible);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnABC);
+            this.Controls.Add(this.btnNo);
+            this.Controls.Add(this.btnSi);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSi;
+        private System.Windows.Forms.Button btnNo;
+        private System.Windows.Forms.Button btnABC;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btninvisible;
+        private System.Windows.Forms.Button btnvisible;
+        private System.Windows.Forms.Button btnEjercicio1_3;
+        private System.Windows.Forms.Button btnEjer1_2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnDespedida;
+    }
+}
+
---
+++ Demo/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int boton;
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnEjer1_2_Click(object sender, EventArgs e)
+        {
+            boton = 1;
+            seleccionEjercicio(boton);
+
+        }
+
+        public void seleccionEjercicio(int boton)
+        {
+            switch (boton)
+            {
+                case 1:
+                    btninvisible.Hide();
+                    btnvisible.Hide();
+                    btnDespedida.Hide();
+                    btnABC.Show();
+                    btnNo.Show();
+                    btnSi.Show();
+                    break;
+
+                case 2:
+                    btninvisible.Hide();
+                    btnvisible.Hide();
+                    btnDespedida.Show();
+                    btnABC.Hide();
+                    btnNo.Hide();
+                    btnSi.Hide();
+                    break;
+                case 3:
+                    btninvisible.Show();
+                    btnvisible.Show();
+                    btnDespedida.Hide();
+                    btnABC.Hide();
+                    btnNo.Hide();
+                    btnSi.Hide();
+                    break;
+            }
+        }
+
+        private void btnEjercicio1_3_Click(object sender, EventArgs e)
+        {
+            boton = 2;
+            seleccionEjercicio(boton);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            boton = 3;
+            seleccionEjercicio(boton);
+        }
+
+        private void btnDespedida_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Adios mundo");
+        }
+
+        private void btnvisible_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+        }
+
+        private void btninvisible_Click(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+        }
+
+        private void btnSi_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Si";
+            label2.Text = "Si";
+            label3.Text = "Si";
+        }
+
+        private void btnNo_Click(object sender, EventArgs e)
+        {
+            label1.Text = "No";
+            label2.Text = "No";
+            label3.Text = "No"; 
+        }
+
+        private void btnABC_Click(object sender, EventArgs e)
+        {
+            label1.Text = "A";
+            label2.Text = "B";
+            label3.Text = "C";
+        }
+
+
+
+    }
+}
